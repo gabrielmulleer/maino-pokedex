a5c221c7630119f4748ea58be2e8f8c11723d2a0
gabrielmulleer <emaildegabrielm@gmail.com>
2024-04-21 15:35:37 -0300

    feat: creation of interfaces for typing

---
+++ src/types/pokedex.ts
@@ -0,0 +1,38 @@
+export interface State {
+  pokemons?: Pokemons;
+  visited_pokemons: PokemonDetails[];
+  active_card?: PokemonDetails;
+}
+export interface Pokemons {
+  count: number;
+  next: string | null;
+  previous: string | null;
+  results: PokemonsResult[];
+}
+export interface PokemonsResult {
+  name: string;
+  url: string;
+}
+export interface PokemonDetails {
+  id: number;
+  name: string;
+  moves: PokemonMoves[];
+  game_indices: PokemonGameIndices[];
+  types: PokemonTypes[];
+  evolution_chain: string;
+  evolution: PokemonEvolution;
+  evolutions: string[];
+}
+export interface PokemonEvolution {
+  evolves_to: PokemonEvolution[];
+  species: PokemonsResult;
+}
+interface PokemonMoves {
+  move: PokemonsResult;
+}
+interface PokemonGameIndices {
+  version: PokemonsResult;
+}
+interface PokemonTypes {
+  type: PokemonsResult;
+}
